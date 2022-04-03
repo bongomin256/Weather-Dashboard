ea06c65789d6541b4b6414b89dc258a06ddadf45
Godfrey Bongomin <bongomin@uw.edu>
2022-04-02 20:41:33 -0700

    Creating the global scoops variables in script file and deleting a class name from html file

---
+++ script.js
@@ -0,0 +1,21 @@
+// search section variable
+var cityNameInput = document.querySelector('#city-input')
+var searchBtn = document.querySelector('#search')
+var searchHistoryEl = document.querySelector('.search-history')
+
+// the current weather section variable
+var cityName = document.querySelector('#city-name')
+var dateEl = document.querySelector('#date')
+var weatherIconEl = document.querySelector('#weather-icon')
+var temperatureEl = document.querySelector('#temp')
+var windSpeedEl = document.querySelector('#wind-speed')
+var humidityEl = document.querySelector('#humidity')
+var uvIndexEl = document.querySelector('#uv-index')
+
+// 5-days weather forecast section variable
+var dateElement = document.querySelectorAll('.date')
+var weatherIcon = document.querySelectorAll('.weather-icon')
+var  tempEl= document.querySelectorAll('.temp')
+var  windEl= document.querySelectorAll('.wind-speed')
+var  humidEl= document.querySelectorAll('.humidity')
+
